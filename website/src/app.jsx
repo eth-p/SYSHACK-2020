2646d03fddf7177bfa7c5429f57fecfad28aed4d
Ethan P <eth-p+git@hidden.email>
2020-02-08 11:16:34 -0800

    website: Add react template

---
+++ website/src/app.jsx
@@ -0,0 +1,35 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+import React from "react";
+import {hot} from 'react-hot-loader/root';
+import {useParams} from "react-router";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import * as Storage from "./pages/Storage";
+
+function route(module, route) {
+	function Child() {
+		const params = useParams();
+		const controller = new module.Controller(params);
+		const Context = module.Controller.Context;
+		return <Context.Provider value={controller}><module.default/></Context.Provider>;
+	}
+
+	return <Route path={route} children={<Child/>}/>;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Component
+// ---------------------------------------------------------------------------------------------------------------------
+
+function App() {
+	return (
+		<Router>
+			<Switch>
+				{route(Storage, '/me/:snowflake')}
+			</Switch>
+		</Router>
+	);
+}
+
+export default hot(App);
---
+++ website/src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import "./index.scss";
+
+const root = document.getElementById("app");
+ReactDOM.render(<App/>, root);
